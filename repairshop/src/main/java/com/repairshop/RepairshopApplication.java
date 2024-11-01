f489588a39150240a6c2d9ce7bab93107aed61a4
harshit <8708440748y@gmail.com>
2024-11-01 14:21:09 +0530

    repairshop repository initialized

---
+++ repairshop/src/main/java/com/repairshop/RepairshopApplication.java
@@ -0,0 +1,13 @@
+package com.repairshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairshopApplication.class, args);
+	}
+
+}
---
+++ repairshop/src/test/java/com/repairshop/RepairshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
